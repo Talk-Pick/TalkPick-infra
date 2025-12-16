16b9e305d3c24aed90a2b6d197459a0ebb0eb37d
hszoo <trre1827151@gmail.com>
2025-12-17 01:43:06 +0900

    feat(nlb): add network load balancer for private EC2

--- main.tf
+++ main.tf
@@ -15,6 +15,12 @@ locals {
       "ap-northeast-2b"
     ]
   }
+  nlb={
+    name: "talkpick-nlb",
+    tg_name: "talkpick-nlb-tg",
+    target_port: 80,
+    listener_port: 80
+  }
 }
 
 // VPC Module
@@ -27,4 +33,23 @@ module "vpc" {
   public_subnet_cidrs = local.vpc.public_cidrs
   private_subnet_cidrs = local.vpc.private_cidrs
   availability_zones = local.vpc.availability_zones
+}
+
+// NLB Module
+module "nlb" {
+  // refer ./terraform-nlb module
+  source = "./terraform-nlb"
+
+  // environments from locals
+  nlb_name       = local.nlb.name         // nlb name
+  target_name        = local.nlb.tg_name  // target group name
+  target_port    = local.nlb.target_port
+  listener_port    = local.nlb.listener_port
+
+  // from terraform-vpc
+  vpc_id         = module.terraform-vpc-aws.talkpick_vpc_id
+  public_subnet_ids = module.terraform-vpc-aws.talkpick_public_subnet_ids
+
+  // from terraform-ec2
+  ec2_private_ips = module.terraform-ec2.talkpick_private_ec2_private_ips
 }
---
+++ terraform-nlb/variables.tf
@@ -0,0 +1,40 @@
+variable "name_prefix" {
+  description = "Cash Cow default prefix"
+  type        = string
+  default     = "ccw"
+}
+
+variable "nlb_name" {
+  description = "NLB name"
+  type = string
+}
+
+variable "target_name" {
+  description = "NLB Target group name"
+  type = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of Public Subnets"
+  type = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC id"
+  type = string
+}
+
+variable "target_port" {
+  description = "NLB target port"
+  type = number
+}
+
+variable "listener_port" {
+  description = "NLB listen port"
+  type = number
+}
+
+variable "ec2_private_ips" {
+  description = "List of EC2 private IPs to attach to the target group"
+  type        = list(string)
+}
